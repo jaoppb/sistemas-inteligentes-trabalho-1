8d8a9b9f0d2968db5efdf34684ea5be0750fed6a
jaoppb <joaopedroperes06@gmail.com>
2025-11-08 15:38:06 -0300

    feat: statistics

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,9 +1,11 @@
+import argparse
+import sys
 import io
+import pandas as pd
 
 import encode
 import countries
 import states
-import pandas as pd
 
 
 def remove_columns(data: pd.DataFrame, columns: list[str]):
@@ -27,10 +29,52 @@ def parse_data(file_base_name):
         data.to_csv(file, index=False, sep=";")
 
 
-def main():
+def parse():
     for file_base_name in ["EXP_2025", "IMP_2025"]:
         parse_data(file_base_name)
 
 
+def read_processed(file_base_name):
+    with open(f"data/{file_base_name}_processed.csv", "r", encoding="utf-8") as file:
+        data = pd.read_csv(file, sep=";")
+    return data
+
+
+def print_statistics(data: pd.DataFrame):
+    value_sum = data["VL_FOB"].sum()
+    value_avg = data["VL_FOB"].mean()
+    value_mode = data["VL_FOB"].mode()[0]
+    value_deviation = data["VL_FOB"].std()
+    print(f"Total VL_FOB: {value_sum}")
+    print(f"Average VL_FOB: {value_avg}")
+    print(f"Mode VL_FOB: {value_mode}")
+    print(f"Standard Deviation VL_FOB: {value_deviation}")
+
+
+def statistics():
+    for file_base_name in ["EXP_2025", "IMP_2025"]:
+        print(f"Statistics for {file_base_name}_processed.csv:")
+        data = read_processed(file_base_name)
+        print_statistics(data)
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Process and analyze trade data files.",
+        epilog=f"Example usage: python {sys.argv[0]} parse",
+    )
+
+    parser.add_argument(
+        "command",
+        choices=["parse", "statistics"],
+        help="The command to execute.",
+    )
+
+    args = parser.parse_args()
+    if args.command == "parse":
+        parse()
+    elif args.command == "statistics":
+        statistics()
+    else:
+        parser.print_help()
+        sys.exit(1)
--- scripts/states.py
+++ scripts/states.py
@@ -1,8 +1,20 @@
 import pandas as pd
+import unicodedata
 
 states_data = pd.read_csv("data/estados.csv", delimiter=";")
 
-states = {each["SIGLA_UF"]: each["NOME_UF"] for each in states_data.to_dict("records")}
+
+def remove_accents(text):
+    if not isinstance(text, str):
+        return text
+    nfd = unicodedata.normalize("NFD", text)
+    return "".join([char for char in nfd if unicodedata.category(char) != "Mn"]).upper()
+
+
+states = {
+    each["SIGLA_UF"]: remove_accents(each["NOME_UF"])
+    for each in states_data.to_dict("records")
+}
 
 
 def parse_state(code):
